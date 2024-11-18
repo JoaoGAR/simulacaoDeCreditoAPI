d2c1180c897677879671c0e53518831fba9b3c5b
JoaoGAR <jgarchanjo@gmail.com>
2024-11-18 10:46:07 -0300

    merge

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('/simulacao/credito', [App\Http\Controllers\api\V1\InstituicoesController::class, 'simularCrédito']);
-    Route::post('/simulacao/oferta', [App\Http\Controllers\api\V1\OfertasCreditoController::class, 'simularOferta']);
-    Route::post('/simulacao/oferta/melhorOpcao', [App\Http\Controllers\api\V1\OfertasCreditoController::class, 'melhoresOfertas']);
+    Route::post('/simulacao/credito', [App\Http\Controllers\Api\V1\InstituicoesController::class, 'simularCrédito']);
+    Route::post('/simulacao/oferta', [App\Http\Controllers\Api\V1\OfertasCreditoController::class, 'simularOferta']);
+    Route::post('/simulacao/oferta/melhorOpcao', [App\Http\Controllers\Api\V1\OfertasCreditoController::class, 'melhoresOfertas']);
 });
